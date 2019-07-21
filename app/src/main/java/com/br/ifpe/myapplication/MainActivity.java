ff37a7d3cb6e6d4a57277845ffa75287946da2d8
adm24help <24helpapp@gmail.com>
2019-07-21 16:34:22 -0300

    inicio layout 24 help

--- app/src/main/java/com/br/ifpe/myapplication/MainActivity.java
+++ app/src/main/java/com/br/ifpe/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference firebaseRef = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference usuarioeRef = firebaseRef.child("usuario");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +21,18 @@ public class MainActivity extends AppCompatActivity {
         //Gravando
         //Criando a estrutura Usuario> Nome e setando o valor Waltson
         firebaseRef.child("usuario").child("nome").setValue("Waltson");
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNome("Gilson");
+        usuario.setEmail("gilson@gmail.com");
+        usuario.setSexo("M");
+        usuario.setStatus("0");
+
+        //Gravando um obsjeto usuario
+        usuarioeRef.child("U001").setValue(usuario);
+
+
+
     }
 }
--- app/src/main/java/com/br/ifpe/myapplication/Usuario.java
+++ app/src/main/java/com/br/ifpe/myapplication/Usuario.java
@@ -1,4 +1,83 @@
 package com.br.ifpe.myapplication;
 
 public class Usuario {
+
+    private String id;
+    private String nome;
+    private String sexo;
+    private String email;
+    private String senha;
+    private String status;
+    private String profissao;
+    private String apresentacao;
+
+
+public Usuario (){
+
+};
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getApresentacao() {
+        return apresentacao;
+    }
+
+    public void setApresentacao(String apresentacao) {
+        this.apresentacao = apresentacao;
+    }
 }
